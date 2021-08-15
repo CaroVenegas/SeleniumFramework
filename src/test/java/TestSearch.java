46bde3921744650ef6fe65ce5ddda17c19b040af
caro <carolina.benavides.venegas@ucreativa.com>
2021-08-15 08:57:02 -0600

    Avance de la semana 4 clase 7

--- src/test/java/TestSearch.java
+++ src/test/java/TestSearch.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-
 public class TestSearch extends BaseClass{
     @Test
     @Parameters({"searchCriteria", "expectedResult"})
