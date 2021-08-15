d8ba37141fef9e2594e7b1cc8bea5871b4998101
caro <carolina.benavides.venegas@ucreativa.com>
2021-08-15 10:39:55 -0600

    Semana 5 clase 8

---
+++ src/test/java/demo/TestWithData.java
@@ -0,0 +1,34 @@
+package demo;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestWithData {
+
+    @Test(dataProvider = "numeroProvider")
+    public void Suma(int numero){
+        Assert.assertTrue(numero > 0);
+        System.out.println("Numero: " + numero);
+    }
+
+    @DataProvider(name = "numeroProvider")
+    public Object[][] methodNumeroProvider(){
+        return new Object[][]{
+                {5}, {6}, {-6}
+        };
+    }
+}
+
+
+/**
+ *
+ * Nombre Correo    Apellido
+ * carolina   carobv2096@hotmail.com  Benavides
+ * carolina carobv2096@...  Venegas
+ *
+ *
+ * Numero
+ * 5
+ * 6
+ * */
